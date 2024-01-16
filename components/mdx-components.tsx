68b2a1f7dc0b05d8a8cf5374fddc42dd7eb58cc5
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-01-15 22:41:26 -0700

    docs: add link to github issue to solve image component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
 const components = {
+  // https://github.com/hashicorp/next-mdx-remote/issues/405
   Image: (props: React.ComponentProps<typeof Image>) => (
     <Image {...props} alt={props.alt} />
   ),
