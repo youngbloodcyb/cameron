65d33c630df81211e6a848dfaa2dd0139d4de056
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-01-27 23:06:15 -0700

    update styles

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,11 +4,10 @@ import { compareDesc } from "date-fns";
 
 function PostCard(post: Post, index: number) {
   return (
-    <Link href={post.url} className="grid grid-cols-4 max-w-[400px]">
-      <h2>01</h2>
-      <h2>{post.title}</h2>
-      <h2></h2>
-      <h2>2023</h2>
+    <Link href={post.url} className="grid grid-cols-[1fr_6fr_1fr]">
+      <h2 className="col-span-1">01</h2>
+      <h2 className="col-span-1">{post.title}</h2>
+      <h2 className="col-span-1">2023</h2>
     </Link>
   );
 }
@@ -20,14 +19,18 @@ function page() {
 
   return (
     <>
-      <section className="flex flex-col">
-        <div>
+      <section className="flex flex-row">
+        <div className="flex flex-col justify-between col-span-1 w-1/2">
+          <div>
+            <h2>Cameron Youngblood</h2>
+          </div>
           <div className="space-y-2">
             {posts.map((post, idx) => (
               <PostCard key={idx} index={idx} {...post} />
             ))}
           </div>
         </div>
+        <div></div>
       </section>
     </>
   );
