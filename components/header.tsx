e24bdc53cffa19ecdd481d83913d6d2f17f526ce
Bridger Tower <57158102+brijr@users.noreply.github.com>
2024-10-08 14:44:50 -0600

    make Logo a direct import so will always show up

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ModeToggle } from "./theme-toggle";
 import Image from "next/image";
+import Logo from "@/public/logo.svg";
 
 export default function Header() {
   return (
@@ -8,7 +9,7 @@ export default function Header() {
       <div className="flex items-center justify-between py-4">
         <Link className="flex font-black uppercase" href="/">
           <span className="sr-only">9D8</span>
-          <Image width={25} height={25} src="logo.svg" alt="cameron.so"></Image>
+          <Image width={25} height={25} src={Logo} alt="cameron.so"></Image>
         </Link>
         <div className="flex items-center justify-between gap-2">
           <Link href="https://github.com/9d8dev">
