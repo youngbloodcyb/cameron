fdfe87a1848361d9fad823731b4a2501d12c2c82
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-11-19 15:45:03 -0800

    change images and change image component

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -16,8 +16,8 @@ export default function MDXImage({ caption, alt, ...props }: MDXImageProps) {
   const href = props.src.toString();
 
   return (
-    <motion.a className="my-6 flex cursor-pointer flex-col justify-end gap-2" href={href} whileHover={{ scale: 0.975, opacity: 0.9 }}>
-      <div className="relative max-h-96 w-full overflow-hidden rounded-large border border-border">
+    <motion.div className="my-6 flex flex-col justify-end gap-2">
+      <div className="relative max-h-96 w-full overflow-hidden rounded-md border border-border">
         <Image
           unoptimized
           alt={alt}
@@ -37,6 +37,6 @@ export default function MDXImage({ caption, alt, ...props }: MDXImageProps) {
         />
       </div>
       {caption && <sub className="pt-2 text-center">{caption}</sub>}
-    </motion.a>
+    </motion.div>
   );
 }
