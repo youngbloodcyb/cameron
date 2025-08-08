7835171bbe5f0ef6e286d2e0f4b9879ee060e6b1
youngbloodcyb <cameron.youngblood@gmail.com>
2025-08-07 22:46:40 -0700

    update

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <FadeIn.Item>
         <p>build, sell, surf</p>
         <p>
-          Currently at{" "}
+          engineering ai systems for gtm at{" "}
           <Link href="https://vercel.com" className="underline" target="_blank">
             Vercel
           </Link>
